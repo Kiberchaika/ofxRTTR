c2241a237d9acf45dc5a702bf0fc7c1c2b7d5e85
andorxornot <andorxornot@gmail.com>
2020-09-18 20:01:38 +0300

    Added helper function

--- src/ofxRTTR.h
+++ src/ofxRTTR.h
@@ -13,3 +13,12 @@
 #include "from_json.h"
 
 using namespace rttr;
+
+namespace io
+{
+	size_t get_obj_properties_count(const instance& obj2)
+	{
+		instance obj = obj2.get_type().get_raw_type().is_wrapper() ? obj2.get_wrapped_instance() : obj2;
+		return obj.get_derived_type().get_properties().size();
+	}
+}
